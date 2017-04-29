a0068bd8ed81e1d007dd22c450704bf842a6dc75
Ridhima Mohindra <Ridhimamohindra06@gmail.com>
2017-04-29 09:40:59 +0530

    Added 2 new arrays

---
+++ arraydemo3.java
@@ -0,0 +1,42 @@
+//to remove duplicate elements from arrays
+import java.io.*;
+import java.util.*;
+class arraydemo3{
+public static void main(String srgs[])throws IOException{
+InputStreamRedaer ins=new InputStreamReader(System.in);
+BufferedReader br=new BufferedReader(ins);
+int m,k;
+System.out.println("Enter number of  elemnts in the array");
+int n=Integer.parseInt(br.readLine());
+int a[]=new int[n];
+System.out.println("Enter the Elemnts of the array");
+for(int i=0;i<n;i++){
+System.out.println("Enter the Element");
+a[i]=Integer.parseInt(br.readLine());
+m=n;
+}
+for(int i=0;i<m;i++){
+for(int j=0;j<m;j++){
+if(i==j)
+continue;
+else if(a[i]==a[j]){
+k=j;
+m--;
+while(k<m){
+a[k]=a[k+1];
+k++;
+}
+j==0;
+}
+}
+}
+System.out.println("After removing duplicate arrays ... list becomes");
+for(int i=0;i<m;i++){
+System.out.println("Element at "+i+" is "+a[i]);}
+System.out.println("total elements removed are "n-m);
+}
+}
+
+}
+
+
